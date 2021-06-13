e4a2815ebf1b5b0b4ed7f9d8b182eb54a767bd7f
keyfpenc11 <cstylerb@gmail.com>
2021-06-13 23:44:36 +0300

    First fullfetch release!

--- fullfetch.sh
+++ fullfetch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #TROLLED by keyfpenc11 with lulz))
 
+echo Fullfetch v1.0
+echo First release!
+echo
 echo Obtaining system info...
 sleep 1s
 echo Calculating...
 sleep 1.7458s
 echo
 inxi -b
-echo 
+echo
 echo Successfull!
+echo
